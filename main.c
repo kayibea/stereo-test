cc0a7859db2ff6d51424fa5a4059b8727f170d85
kayibea <kayibea@proton.me>
2026-02-08 07:53:00 +0100

    refactor(main.c): simplify code

--- main.c
+++ main.c
@@ -1,177 +1,87 @@
 #define _GNU_SOURCE
 #include <alsa/asoundlib.h>
-#include <ctype.h>
-#include <errno.h>
 #include <math.h>
 #include <signal.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
-enum {
-  CHANNELS = 2,
-  SAMPLE_RATE = 48000,
-  AMPLITUDE = 30000,
-  DURATION_SEC = 1,
-  NUM_BUFFERS = 3  // left, right, dual
-};
+#define RATE 48000
+#define AMP 30000
+#define FREQ 440.0
+#define DUR 1
+#define CHANS 2
 
 static volatile int running = 1;
-static const double FREQUENCY_HZ = 440.0;
-typedef enum { CHANNEL_LEFT = 0, CHANNEL_RIGHT = 1, CHANNEL_DUAL = 2 } channel_t;
 
-typedef struct {
-  int16_t *data[NUM_BUFFERS];
-  int frames;
-} audio_buffers_t;
-
-static void handle_signal(int sig, siginfo_t *info, void *ctx) {
-  (void)ctx;
-  (void)info;
+void handle_signal(int sig) {
+  (void)sig;
   running = 0;
-  printf("\nCaught signal %d\n", sig);
 }
 
-static inline int16_t sine_sample(int index) {
-  return (int16_t)(sin(2.0 * M_PI * FREQUENCY_HZ * index / SAMPLE_RATE) * AMPLITUDE);
+snd_pcm_t *init_pcm() {
+  snd_pcm_t *pcm;
+  if (snd_pcm_open(&pcm, "default", SND_PCM_STREAM_PLAYBACK, 0) < 0) return NULL;
+
+  int err = snd_pcm_set_params(pcm, SND_PCM_FORMAT_S16_LE, SND_PCM_ACCESS_RW_INTERLEAVED, CHANS,
+                               RATE, 1, 1000000);
+  if (err < 0) {
+    snd_pcm_close(pcm);
+    return NULL;
+  }
+  return pcm;
 }
 
-static void generate_buffers(audio_buffers_t *b) {
-  int frames = b->frames;
+void play(snd_pcm_t *pcm, int16_t *data, int frames, char mode) {
+  int off = 0;
+  int16_t frame_chunk[512 * CHANS];
 
-  int16_t *left = b->data[CHANNEL_LEFT];
-  int16_t *right = b->data[CHANNEL_RIGHT];
-  int16_t *dual = b->data[CHANNEL_DUAL];
+  while (running && off < frames) {
+    int chunk_size = (frames - off > 512) ? 512 : (frames - off);
+
+    for (int i = 0; i < chunk_size; i++) {
+      int16_t s = data[off + i];
+      frame_chunk[i * 2] = (mode == 'r') ? 0 : s;
+      frame_chunk[i * 2 + 1] = (mode == 'l') ? 0 : s;
+    }
+
+    snd_pcm_sframes_t ret = snd_pcm_writei(pcm, frame_chunk, chunk_size);
+    if (ret < 0) {
+      if (snd_pcm_recover(pcm, ret, 1) < 0) break;
+    } else {
+      off += ret;
+    }
+  }
+
+  snd_pcm_drain(pcm);
+  snd_pcm_prepare(pcm);
+}
+
+int main() {
+  signal(SIGINT, handle_signal);
+  snd_pcm_t *pcm = init_pcm();
+  if (!pcm) return 1;
+
+  int frames = RATE * DUR;
+  int16_t *samples = malloc(frames * sizeof(int16_t));
 
   for (int i = 0; i < frames; i++) {
-    int16_t s = sine_sample(i);
-    int idx = i * CHANNELS;
-
-    // left
-    left[idx + 0] = s;
-    left[idx + 1] = 0;
-
-    // right
-    right[idx + 0] = 0;
-    right[idx + 1] = s;
-
-    // dual
-    dual[idx + 0] = s;
-    dual[idx + 1] = s;
-  }
-}
-
-static int play_buffer(snd_pcm_t *pcm, int16_t *buf, int frames) {
-  int frames_written = 0;
-  while (frames_written < frames) {
-    ssize_t ret = snd_pcm_writei(pcm, buf + frames_written * CHANNELS, frames - frames_written);
-    if (ret < 0) {
-      if (ret == -EPIPE) {  // underrun
-        snd_pcm_prepare(pcm);
-      } else if (ret == -ESTRPIPE) {  // suspend
-        while ((ret = snd_pcm_resume(pcm)) == -EAGAIN) sleep(1);
-        if (ret < 0) snd_pcm_prepare(pcm);
-      } else {
-        fprintf(stderr, "ALSA write error: %s\n", snd_strerror(ret));
-        return -1;
-      }
-    } else {
-      frames_written += ret;
-    }
-  }
-  return 0;
-}
-
-static int init_pcm(snd_pcm_t **pcm) {
-  snd_pcm_hw_params_t *params;
-
-  if (snd_pcm_open(pcm, "default", SND_PCM_STREAM_PLAYBACK, 0) < 0) {
-    fprintf(stderr, "Failed to open PCM device\n");
-    return -1;
+    samples[i] = (int16_t)(sin(2.0 * M_PI * FREQ * i / RATE) * AMP);
   }
 
-  snd_pcm_hw_params_alloca(&params);
-  snd_pcm_hw_params_any(*pcm, params);
-  snd_pcm_hw_params_set_access(*pcm, params, SND_PCM_ACCESS_RW_INTERLEAVED);
-  snd_pcm_hw_params_set_format(*pcm, params, SND_PCM_FORMAT_S16_LE);
-  snd_pcm_hw_params_set_rate(*pcm, params, SAMPLE_RATE, 0);
-  snd_pcm_hw_params_set_channels(*pcm, params, CHANNELS);
-
-  if (snd_pcm_hw_params(*pcm, params) < 0) {
-    fprintf(stderr, "Failed to set PCM parameters\n");
-    snd_pcm_close(*pcm);
-    return -1;
-  }
-
-  return 0;
-}
-
-int main(void) {
-  struct sigaction sa = {0};
-  sa.sa_sigaction = handle_signal;
-  sa.sa_flags = SA_SIGINFO;
-  sigemptyset(&sa.sa_mask);
-  sigaction(SIGINT, &sa, NULL);
-  sigaction(SIGTERM, &sa, NULL);
-
-  snd_pcm_t *pcm;
-  if (init_pcm(&pcm) < 0) return 1;
-
-  int frames = SAMPLE_RATE * DURATION_SEC;
-  audio_buffers_t buffers = {0};
-  buffers.frames = frames;
-
-  for (int i = 0; i < NUM_BUFFERS; i++) {
-    buffers.data[i] = malloc(frames * CHANNELS * sizeof(int16_t));
-    if (!buffers.data[i]) {
-      fprintf(stderr, "Failed to allocate buffer %d: %s\n", i, strerror(errno));
-      for (int j = 0; j < i; j++) free(buffers.data[j]);
-      snd_pcm_close(pcm);
-      return 1;
-    }
-  }
-
-  generate_buffers(&buffers);
-
-  printf("pid: %d\n(q) to quit\n(l) for left\n(r) for right\n(d) for both\n", getpid());
-
+  printf("Commands: [l]eft, [r]ight, [d]ual, [q]uit\n");
   while (running) {
     printf("> ");
     fflush(stdout);
-
     int c = getchar();
-    if (c == EOF) break;
-    if (isspace(c)) continue;
+    if (c == 'q' || c == EOF) break;
+    if (c == '\n') continue;
 
-    c = tolower(c);
+    if (strchr("lrd", c)) play(pcm, samples, frames, c);
 
-    switch (c) {
-      case 'q':
-        running = 0;
-        break;
-      case 'l':
-        printf("Playing left channel...\n");
-        play_buffer(pcm, buffers.data[CHANNEL_LEFT], frames);
-        break;
-      case 'r':
-        printf("Playing right channel...\n");
-        play_buffer(pcm, buffers.data[CHANNEL_RIGHT], frames);
-        break;
-      case 'd':
-        printf("Playing both channels...\n");
-        play_buffer(pcm, buffers.data[CHANNEL_DUAL], frames);
-        break;
-      default:
-        printf("Invalid command: '%c'\n", c);
-    }
-
-    int junk;
-    while ((junk = getchar()) != '\n' && junk != EOF);
+    while (getchar() != '\n');
   }
 
-  for (int i = 0; i < NUM_BUFFERS; i++) free(buffers.data[i]);
+  free(samples);
   snd_pcm_close(pcm);
   return 0;
 }
